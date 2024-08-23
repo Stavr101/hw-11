61f691dc8bc45bc68ebae126959e18c7711915c1
Sergey Shishkin <Shishkin_SU@ukr.net>
2024-08-23 11:14:28 +0300

    add infiniti scroll

--- src/index.js
+++ src/index.js
@@ -10,13 +10,40 @@ const refs = {
   galleryImg: document.querySelector(".gallery"),
   photo: document.querySelector(".photo-card a"),
   buttonLoadMore: document.querySelector(".load-more"),
+  target: document.querySelector(".js-guard"),
 };
 
-const { form, input, galleryImg, photo, buttonLoadMore } = refs;
+const { form, input, galleryImg, photo, buttonLoadMore, target } = refs;
 
 let currentPage = 1;
-console.log("🚀 ~ currentPage:", currentPage);
+
 let searchQuery = "";
+let options = {
+  root: null,
+  rootMargin: "200px",
+  threshold: 1.0,
+};
+
+let observer = new IntersectionObserver(onLoad, options);
+
+function onLoad(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      currentPage += 1;
+      getPosts(searchQuery, currentPage).then((data) => {
+        if (data.total === data.totalHits) {
+          buttonLoadMore.hidden = true;
+          Notify.failure(
+            "We're sorry, but you've reached the end of search results."
+          );
+        }
+
+        galleryImg.insertAdjacentHTML("beforeend", markupPosts(data));
+        gallery();
+      });
+    }
+  });
+}
 
 const onSearch = (event) => {
   event.preventDefault();
@@ -33,28 +60,29 @@ const onSearch = (event) => {
         Notify.success(`Hooray! We found ${data.total} images.`);
       }
       if (data.hits.length * currentPage !== data.totalHits) {
-        buttonLoadMore.hidden = false;
+        // buttonLoadMore.hidden = false;
       }
       galleryImg.innerHTML = markupPosts(data);
+      observer.observe(target);
       gallery();
     })
     .catch((error) => console.log(error))
     .finally(form.reset());
 };
-const onLoad = () => {
-  currentPage += 1;
-  buttonLoadMore.hidden = false;
-  getPosts(searchQuery, currentPage).then((data) => {
-    if (data.total === data.totalHits) {
-      buttonLoadMore.hidden = true;
-      Notify.failure(
-        "We're sorry, but you've reached the end of search results."
-      );
-    }
-    galleryImg.insertAdjacentHTML("beforeend", markupPosts(data));
-    gallery();
-  });
-};
+// const onLoad = () => {
+//   currentPage += 1;
+//   buttonLoadMore.hidden = false;
+//   getPosts(searchQuery, currentPage).then((data) => {
+//     if (data.total === data.totalHits) {
+//       buttonLoadMore.hidden = true;
+//       Notify.failure(
+//         "We're sorry, but you've reached the end of search results."
+//       );
+//     }
+//     galleryImg.insertAdjacentHTML("beforeend", markupPosts(data));
+//     gallery();
+//   });
+// };
 
 form.addEventListener("submit", onSearch);
-buttonLoadMore.addEventListener("click", onLoad);
+// buttonLoadMore.addEventListener("click", onLoad);
